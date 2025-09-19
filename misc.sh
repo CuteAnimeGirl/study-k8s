926cb58eceb40ca57a7c2fd9556be1327dd7864d
CuteAnimeGirl <voroninaleksandr98@gmail.com>
2025-09-19 14:47:10 +0700

    auto

--- misc.sh
+++ misc.sh
@@ -3,8 +3,7 @@
 ## Metallb ##
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.15.2/config/manifests/metallb-native.yaml
-# or
-kubectl apply -f metallb-native.yaml
+# or kubectl apply -f metallb-native.yaml
 
 kubectl -n metallb-system get pods
 
@@ -13,15 +12,18 @@ kubectl apply -f metallb-config.yaml
 ## ingress-nginx ##
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.13.2/deploy/static/provider/cloud/deploy.yaml
-# or
-kubectl apply -f ingress-nginx.yaml
+# or kubectl apply -f ingress-nginx.yaml
 
 kubectl get pods -n ingress-nginx
 kubectl get svc -n ingress-nginx
 
+## Storage ##
+
+kubectl apply -f storage-class.yaml
+
 ## app ##
 
-kubectl apply -f test-app.yaml
+kubectl apply -f app1.yaml -f app2.yaml -f app-ingress.yaml
 
-kubectl get pods -l app=test-web-app
-kubectl get svc test-web-app-service
+chmod +x init-pods.sh
+./init-pods.sh
