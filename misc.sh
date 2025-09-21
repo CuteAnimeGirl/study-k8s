968509b87ecc12ca83cbb5df8946dc77f46ccd9d
CuteAnimeGirl <voroninaleksandr98@gmail.com>
2025-09-21 21:04:54 +0700

    auto

--- misc.sh
+++ misc.sh
@@ -20,9 +20,8 @@ kubectl get svc -n ingress-nginx
 ## longhorn ##
 
 longhornctl --kube-config ~/.kube/config --image longhornio/longhorn-cli:v1.9.1 install preflight
-helm repo add longhorn https://charts.longhorn.io && helm repo update
 
-helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace
+helm upgrade --install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace
 
 kubectl get pods -n longhorn-system
 
@@ -33,3 +32,16 @@ kubectl create -f longhorn-custom-storageclass.yaml
 kubectl apply -f app1.yaml -f app2.yaml -f ingress-main.yaml
 #kubectl delete -f app1.yaml -f app2.yaml -f app-ingress.yaml -f storage-class.yaml
 
+kubectl get pods -n longhorn-system -n default
+
+## k8s web ##
+
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+
+kubectl apply -f dashboard-serviceaccount.yaml
+
+kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
+
+kubectl apply -f dashboard-ingress.yaml
