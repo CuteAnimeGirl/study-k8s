4eec43af3fe9a08173662e2773d27944d24fedb9
CuteAnimeGirl <voroninaleksandr98@gmail.com>
2025-09-22 12:32:05 +0700

    auto

--- misc.sh
+++ misc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ## Metallb ##
-helm upgrade --install metallb metallb/metallb \
+helm upgrade --install metallb metallb \
   --repo https://metallb.github.io/metallb \
   --namespace metallb-system --create-namespace \
   --version 0.15.2
 
-kubectl -n metallb-system get pods
+kubectl -n metallb-system get pods -w
 kubectl apply -f metallb-conf.yaml
 
 ## Ingress-nginx ##
@@ -15,7 +15,7 @@ helm upgrade --install ingress-nginx ingress-nginx \
   --namespace ingress-nginx --create-namespace \
   --version 4.13.2
 
-kubectl get pods -n ingress-nginx
+kubectl get pods -n ingress-nginx -w
 kubectl get svc -n ingress-nginx
 
 ## Longhorn ##
@@ -24,23 +24,20 @@ kubectl get svc -n ingress-nginx
 
 ./longhornctl --kube-config ~/.kube/config --image longhornio/longhorn-cli:v1.9.1 install preflight
 
-helm upgrade --install longhorn longhorn/longhorn \
+helm upgrade --install longhorn longhorn \
   --repo https://charts.longhorn.io \
   --namespace longhorn-system --create-namespace \
   --version 1.9.1
 
-helm repo add longhorn https://charts.longhorn.io
-helm repo update
-helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace --version 1.9.1
-
-kubectl get pods -n longhorn-system
-kubectl apply -f longhorn-sc1.yaml
+kubectl get pods -n longhorn-system -w
+kubectl apply -f longhorn-sc1.yaml -f longhorn-ingress.yaml
 
 ## Kubernetes Dashboard ##
-
-helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard \
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard \
   --repo https://kubernetes.github.io/dashboard/ \
   --namespace kubernetes-dashboard --create-namespace \
   --version 7.13.0
+
+kubectl get pods -n kubernetes-dashboard -w
 
 kubectl apply -f k8s-dashboard-cluster-admin.yaml -f k8s-dashboard-ingress.yaml
