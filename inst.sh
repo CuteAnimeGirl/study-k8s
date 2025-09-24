2e42332bb1853c2d69002c776a34c3278c77d869
CuteAnimeGirl <voroninaleksandr98@gmail.com>
2025-09-24 21:29:01 +0700

    auto

--- inst.sh
+++ inst.sh
@@ -38,17 +38,13 @@ helm upgrade --install longhorn longhorn \
 kubectl get pods -n longhorn-system -w
 kubectl apply -f longhorn-sc1.yaml -f longhorn-ingress.yaml
 
-## Kubernetes Dashboard ##
-helm upgrade --install kubernetes-dashboard kubernetes-dashboard \
-  --repo https://kubernetes.github.io/dashboard \
-  --namespace kubernetes-dashboard --create-namespace \
-  --version 7.13.0
+## Prometheus Stack ##
+helm upgrade --install prometheus-stack prometheus-community/kube-prometheus-stack \
+  --namespace monitoring --create-namespace \
+  --version 77.11.0 \
+  -f prometheus-stack-values.yaml
 
-kubectl get pods -n kubernetes-dashboard -w
-
-kubectl apply -f k8s-dashboard-cluster-admin.yaml -f k8s-dashboard-ingress.yaml
-
-kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath="{.data.token}" | base64 -d
+kubectl get pods -n monitoring -w
 
 ## Gitlab ##
 
@@ -56,27 +52,8 @@ kubectl apply -f gitlab-namespace.yaml
 
 kubectl create secret generic gitlab-secrets \
   --namespace=gitlab \
-  --from-literal=root-password=test-password-5454!
+  --from-literal=GITLAB_ROOT_PASSWORD=test-password-5454!
 
 kubectl apply -f gitlab-deployment.yaml -f gitlab-service.yaml -f gitlab-ingress.yaml
 
 kubectl get pods -n gitlab -w
-
-## MinIO ##
-
-kubectl apply -f minio-namespace.yaml
-
-kubectl create secret generic minio-secret \
-  --namespace=minio \
-  --from-literal=rootUser=minioadmin \
-  --from-literal=rootPassword=test-password-5454!
-
-helm upgrade --install minio minio \
-  --repo https://charts.min.io/ \
-  --namespace minio --create-namespace \
-  --version 5.4.0 \
-  -f minio-values.yaml
-
-kubectl apply -f minio-ingress.yaml
-
-kubectl get pods -n minio -w
